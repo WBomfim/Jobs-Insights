d12bb95d6f6e99e6977609ac459ffe5b6818d29c
WBomfim <willianbomfim@hotmail.com>
2022-12-11 16:53:32 -0300

    test: Implemantado testes para a função count_ocurrences - Os testes verificam se acontagem das palavras estão corretas e ignorando o case sensitive, também é verificado as condições de chamada da função com parametros invalidos e inexistentes.

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,17 @@
-# from src.pre_built.counter import count_ocurrences
+import pytest
+from src.pre_built.counter import count_ocurrences
+
+PATH = "tests/mocks/jobs.csv"
+COUNT_DEVELOPER_WORD = 3
 
 
 def test_counter():
-    pass
+    assert count_ocurrences(PATH, "developer") == COUNT_DEVELOPER_WORD
+    assert count_ocurrences(PATH, "DEVELOPER") == COUNT_DEVELOPER_WORD
+
+    with pytest.raises(TypeError):
+        count_ocurrences(PATH)
+        count_ocurrences("developer")
+
+    with pytest.raises(FileNotFoundError):
+        count_ocurrences("jobs_not_found.csv", "developer")

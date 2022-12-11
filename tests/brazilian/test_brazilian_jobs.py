b4fb535649a655c2a62fb9f426c010f1f48334fa
WBomfim <willianbomfim@hotmail.com>
2022-12-11 17:35:50 -0300

    test: Implementado testes para a função read_brazilian_file - Os testes verificam se as chaves dos dicionários são traduzidas do português para i inglês, e também verifica se é lançado um erro ao chamar a função com um parametro inválido

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,19 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+import pytest
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+PATH = "tests/mocks/brazilians_jobs.csv"
 
 
 def test_brazilian_jobs():
-    pass
+    jobs = read_brazilian_file(PATH)
+    for job in jobs:
+        assert "titulo" not in job
+        assert "salario" not in job
+        assert "tipo" not in job
+
+        assert "title" in job
+        assert "salary" in job
+        assert "type" in job
+
+    with pytest.raises(FileNotFoundError):
+        read_brazilian_file("jobs_not_found.csv")
